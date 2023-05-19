fe4d1a3d8826c1f7a62775a8fdce6da557d9eeac
Chris <yuxiaohui0626@gmail.com>
2023-05-19 13:16:22 +0100

    - Rename

--- src/main/java/pers/chris/template/NonWebApplication.java
+++ src/main/java/pers/chris/template/NonWebApplication.java
@@ -12,11 +12,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @Description 非 Web 项目启动类 or 启动后的前置任务
  */
 @SpringBootApplication
-public class NoWebApplication implements ApplicationRunner {
+public class NonWebApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder()
-                .sources(NoWebApplication.class)
+                .sources(NonWebApplication.class)
                 // 指定非 web 模式
                 .web(WebApplicationType.NONE)
                 .run(args);
